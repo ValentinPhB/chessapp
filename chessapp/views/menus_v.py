074d5c05b636610d0ebdba969a5a2a690bc7ab8e
« <>
2021-05-12 17:37:10 +0200

    Views for controller's python file of the same name.

--- chessapp/views/menus_v.py
+++ chessapp/views/menus_v.py
@@ -1,3 +1,5 @@
+from chessapp.utils.clear_screen_u import Clear
+
 
 class MenuView:
     """
@@ -13,6 +15,7 @@ class MenuView:
     # DISPLAY MENU.
     def _display_menu(self):
         print("\n\nChoisissez votre option : \n")
+
         for key, entry in self.menu.items():
             print(f'{key}: {entry}\n')
 
@@ -37,48 +40,63 @@ class MenuView:
     def welcome_continue():
         print("\n\nCONTINUER UN TOURNOI.")
 
-    # WELCOME MESSAGE RAPPORT MENU.
+    # WELCOME MESSAGE REPORT MENU.
     @staticmethod
-    def welcome_rapports():
-        print("\n\nCHOISISSEZ L'OBJET POUR LEQUEL VOUS SOUHAITEZ"
-              " DES INFORMATIONS.")
+    def welcome_reports():
+        print("\n\nCHOISISSEZ L'OBJET POUR LEQUEL VOUS SOUHAITEZ DES INFORMATIONS.")
 
-    # WELCOME TOURNAMENT RAPPORT MENU.
+    # WELCOME TOURNAMENT REPORT MENU.
     @staticmethod
-    def welcome_tournament_rapports():
-        print("\n\nCHOISISSEZ LE TOURNOIS POUR LEQUEL VOUS SOUHAITEZ"
-              " DES INFORMATIONS.")
+    def welcome_tournament_reports():
+        print("\n\nCHOISISSEZ LE TOURNOIS POUR LEQUEL VOUS SOUHAITEZ DES INFORMATIONS.")
 
     # WELCOME MESSAGE ADD PLAYER FROM DATA.
     @staticmethod
     def welcome_add_from_data():
-        print("\n\nAJOUTER UN NOUVEAU JOUEUR AU TOURNOI DEPUIS LA"
-              " BASE DE DONNÉES.")
+        print("\n\nAJOUTER UN NOUVEAU JOUEUR AU TOURNOI DEPUIS LA BASE DE DONNÉES.")
 
     # WELCOME MESSAGE UPDATE GLOBAL RANKING.
     @staticmethod
     def welcome_update_global_ranking():
         print("\n\n\nMODIFIEZ LE CLASSEMENT D'UN JOUEUR :")
 
+    # SAVING MESSAGE.
+    @staticmethod
+    def saving_state():
+        print("\n\n\n\n>> L'ÉTAT ACTUEL DU TOURNOI EST SAUVEGARDÉ DANS LA BASE DE DONNÉES <<\n")
+
+    # SHOW RANKING TOURNAMENT.
+    @staticmethod
+    def show_ranking_tournament(players_tournament):
+        # CLEAR SCREEN.
+        Clear().screen()
+
+        players_tournament.sort(key=lambda x: (- x.point, x.ranking))
+        print(">> VOICI LE CLASSEMENT POUR CE TOURNOI :\n\n")
+        i = 1
+
+        for player in players_tournament:
+            print(f"PLACE N°{i} : '{player.family_name}, {player.first_name}'"
+                  f" né(e) le : '{player.date_of_birth}',"
+                  f" classement général actuel : '{player.ranking}',"
+                  f" total de points pour ce tournoi : '{player.point}'.")
+            i += 1
+
     # TOOLS TO INTERACT WITH USER
     @staticmethod
     def no_players_db():
-        print("\n> INFORMATION : Il n'y a actuellement aucun joueur"
-              " dans la base de données que vous pourriez ajouter.")
-        print("> Tout joueur déjà ajouté à ce présent tournoi ne peut"
-              " être affiché.")
-        print("> Choisissez la clé '1' pour créer un nouveau joueur et"
-              " l'enregistrer dans la base de données.")
+        print("\n> INFORMATION : Il n'y a actuellement aucun joueur dans la base de données "
+              "que vous pourriez ajouter.")
+        print("> Tout joueur déjà ajouté à ce présent tournoi ne peut être affiché.")
+        print("> Choisissez la clé '1' pour créer un nouveau joueur et l'enregistrer dans la base de données.")
 
     @staticmethod
     def error_entry():
-        print("> INFORMATION : Ici vous devez choisir une des clés"
-              " proposées.\n")
+        print("> INFORMATION : Ici vous devez choisir une des clés proposées.\n")
 
     @staticmethod
     def no_tournament_db():
-        print("\n> INFORMATION : Il n'y a actuellement aucun tournoi"
-              " non-terminé dans la base de données.")
+        print("\n> INFORMATION : Il n'y a actuellement aucun tournoi non-terminé dans la base de données.")
         print("> Vous êtes redirigé sur le menu d'accueil.\n")
 
     @staticmethod
@@ -87,6 +105,5 @@ class MenuView:
 
     @staticmethod
     def none_data():
-        print("\n> INFORMATION : Il n'y a actuellement aucune information"
-              " dans la base de données.")
+        print("\n> INFORMATION : Il n'y a actuellement aucune information dans la base de données.")
         print("> Vous êtes redirigé sur le menu d'accueil.\n")
--- chessapp/views/round_v.py
+++ chessapp/views/round_v.py
@@ -9,8 +9,8 @@ class RoundMakerView:
     def welcome(current_tournament):
         nb_total_round = current_tournament.nb_total_round
         nb_round = len(current_tournament.all_round) + 1
-        print(f"\n\n>>> CRÉATION DU ROUND '{nb_round}' sur"
-              f" '{nb_total_round}' ET DE SES MATCHES. <<<\n")
+
+        print(f"\n\n>>> CRÉATION DU ROUND '{nb_round}' sur '{nb_total_round}' ET DE SES MATCHES. <<<\n")
 
     # WELCOME MESSAGE 2.
     @staticmethod
@@ -18,9 +18,8 @@ class RoundMakerView:
         nb_total_round = current_tournament.nb_total_round
         nb_actual_round = len(current_tournament.all_round)
         control_time = current_tournament.control_time
-        print(f"VOICI LES MATCHS POUR LE ROUND '{nb_actual_round}'"
-              f" sur '{nb_total_round}' "
-              f"QUE VOUS AVEZ GÉNÉRÉ :")
+
+        print(f"VOICI LES MATCHS POUR LE ROUND '{nb_actual_round}' sur '{nb_total_round}' QUE VOUS AVEZ GÉNÉRÉ :")
         print(f"RAPPEL DU CONTRÔLE TIME : '{control_time}'.\n")
 
     # DISPLAY MATCHES.
@@ -28,18 +27,15 @@ class RoundMakerView:
     def display_matches(current_tournament):
         all_players = current_tournament.players_tournament
         list_matches = current_tournament.all_round[-1].matches
+
         i = 1
         for element in list_matches:
             print(f"\n> MATCH {i} :"
-                  f" '{element.first_p.family_name}"
-                  f" {element.first_p.first_name}'"
+                  f" '{element.first_p.family_name} {element.first_p.first_name}'"
                   f" né le : '{element.first_p.date_of_birth}'"
-                  f" classement du tournoi"
-                  f" '{(all_players.index(element.first_p)) + 1}'"
+                  f" classement du tournoi '{(all_players.index(element.first_p)) + 1}'"
                   f" | CONTRE |"
-                  f" '{element.second_p.family_name}"
-                  f" {element.second_p.first_name}'"
+                  f" '{element.second_p.family_name} {element.second_p.first_name}'"
                   f" né le : '{element.second_p.date_of_birth}'"
-                  f" classement du tournoi"
-                  f" '{(all_players.index(element.second_p)) + 1}' ")
+                  f" classement du tournoi '{(all_players.index(element.second_p)) + 1}' ")
             i += 1
